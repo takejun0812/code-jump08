7cb66ef7b61721bc23139e4e5266ef5e6fa8bb96
takejun0812 <junta.takenochi@prum.jp>
2024-06-03 16:01:33 +0900

    add/section reason

--- script.js
+++ script.js
@@ -7,5 +7,18 @@ $(function () {
     });
 });
 
+let winHeight,winScroll,scrollPos;
+$(window).on('load scroll',function(){
+	winScroll = $(window).scrollTop();
+	winHeight = $(window).height();
+	scrollPos = winHeight * 0.9 + winScroll;
+	$(".reason-comment").each(function(){
+		if($(this).offset().top <= scrollPos){
+			$(this).addClass("show");
+		}else{
+			$(this).removeClass("show");
+		}
+	});
+});
 
 Resources

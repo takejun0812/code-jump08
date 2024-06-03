151d0bb5413b0fbe7e83a43f200345217e369401
takejun0812 <junta.takenochi@prum.jp>
2024-06-03 19:48:32 +0900

    add/section review

--- script.js
+++ script.js
@@ -7,18 +7,35 @@ $(function () {
     });
 });
 
-let winHeight,winScroll,scrollPos;
-$(window).on('load scroll',function(){
-	winScroll = $(window).scrollTop();
-	winHeight = $(window).height();
-	scrollPos = winHeight * 0.9 + winScroll;
-	$(".reason-comment").each(function(){
-		if($(this).offset().top <= scrollPos){
-			$(this).addClass("show");
-		}else{
-			$(this).removeClass("show");
-		}
-	});
+let winHeight, winScroll, scrollPos;
+$(window).on('load scroll', function () {
+    winScroll = $(window).scrollTop();
+    winHeight = $(window).height();
+    scrollPos = winHeight * 0.9 + winScroll;
+    $(".reason-comment").each(function () {
+        if ($(this).offset().top <= scrollPos) {
+            $(this).addClass("show");
+        } else {
+            $(this).removeClass("show");
+        }
+    });
+
+    $(window).on('load scroll', function () {
+
+        var box = $('.review-item');
+        var animated = 'animated';
+
+        box.each(function () {
+
+            var boxOffset = $(this).offset().top;
+            var scrollPos = $(window).scrollTop();
+            var wh = $(window).height();
+
+            if (scrollPos > boxOffset - wh + 100) {
+                $(this).addClass(animated);
+            }
+        });
+    });
 });
 
 Resources
